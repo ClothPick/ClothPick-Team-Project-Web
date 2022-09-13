3468a7c727b348b2a17cc953597770738df97adc
김지연 <kdelay20@gmail.com>
2022-09-13 18:40:33 +0900

    [내 옷장] 메인 화면 필터 기능
    - 사용하지 않는 파일 제거
    - token 오류 해결
    - 필터 기능 UI 작업중

---
+++ src/Component/Closet/ClosetFilter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ClosetFilter() {
+  return (
+    <div className="ClosetFilter">
+      <a href="#none" className="on-move">필터 열기</a>
+      <a href="#none" className="off-move">필터 닫기</a>
+    </div>
+  );
+}
+
+export default ClosetFilter;
---
+++ src/Component/Closet/ClosetMain.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Closet.css"
+import ClosetFilter from "./ClosetFilter";
+
+function ClosetMain() {
+  return (
+    <div className="ClosetMain">
+      <div className="header">헤더입니다.</div>
+      <ClosetFilter/>
+    </div>
+
+  );
+}
+
+export default ClosetMain;
