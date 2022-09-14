d6c351c92656833e14a8df7f9252c10002d9012f
김지연 <kdelay20@gmail.com>
2022-09-14 16:03:50 +0900

    [내 옷장] Filter 기능 Toggle
    - react-bootstrap 설치
    - react-icons 설치
    
    - CustomToggle 사용
    - Toggle을 사용함으로서 [필터 열기] 버튼을 누르면 하단에 내용이 나오고 [필터 닫기] 버튼으로 내용과 색상이 검정색으로 바뀐다.
    - [필터 닫기]를 누르면 Toggle이 닫히면서 [필터 열기]로 버튼이 변경되면서 색상이 빨간색으로 바뀐다.

--- src/Component/Closet/ClosetFilter.jsx
+++ src/Component/Closet/ClosetFilter.jsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+// import { CgArrowsShrinkV } from 'react-icons/cg';
+import { CgArrowsMergeAltV } from 'react-icons/cg';
+
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
+
+
+function CustomToggle({ children, eventKey }) {
+
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log('totally custom!'),
+  );
+
+  const [textColor, setTextColor] = useState('black')
+  const [textBorderColor, setTextBorderColor] = useState('black')
+  const [isBlack, setIsBlack] = useState(true);
+  const [contents, setContents] = useState('필터 닫기')
+
+  const changeContents = () => {
+    setContents(prev => prev === '필터 닫기' ? '필터 열기' : '필터 닫기')
+  }
+
+  const handleChangeColor = (e) => {
+    setIsBlack(!isBlack);
+    setTextBorderColor(isBlack ? 'red' : 'black')
+    setTextColor(isBlack ? 'red' : 'black');
+
+  }
+
+  return (
+    <Button
+      type="button"
+      value={isBlack}
+      style={{
+        backgroundColor: 'white',
+        borderWidth: 2,
+        borderColor: textBorderColor
+      }}
+      onClick={() => {
+        decoratedOnClick()
+        handleChangeColor();
+        changeContents();
+      }
+    }
+    >
+      <span style={{color:textColor}}>{contents}<CgArrowsMergeAltV/></span>
+
+      {children}
+    </Button>
+  );
+}
 
 function ClosetFilter() {
   return (
-    <div className="ClosetFilter">
-      <a href="#none" className="on-move">필터 열기</a>
-      <a href="#none" className="off-move">필터 닫기</a>
-    </div>
+    <>
+    <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+        <CustomToggle eventKey="0"></CustomToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+
+    </>
   );
 }
 
