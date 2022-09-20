fa1ef6daa23cadb2c344492fb41a8cbf01283950
김지연 <kdelay20@gmail.com>
2022-09-20 16:05:13 +0900

    [내 옷장] merge
    - 최원영 코드 + 김지연 코드

--- src/Closet/ClosetMain.jsx
+++ src/Closet/ClosetMain.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import "./Closet.css"
-import ClosetFilter from "./ClosetFilter";
-import "./Accordion/Accordion.css";
+import Header from '../Header/header'
+import Sidebar from '../Sidebar/sidebar'
+import ClosetList from './ClosetList/closetList'
+import './CSSClosetMain.css'
 
 // {ClosetFilter} - [내 옷장] Filter 기능
 
 function ClosetMain() {
   return (
-    <div className="ClosetMain">
-      <div className="header">헤더입니다.</div>
-      <ClosetFilter />
+    <div>
+      <Header />
+      <div className="closet-body">
+        <div className="flex"><Sidebar /></div>
+        <div className="flex"><ClosetList /></div>
+
+      </div>
+
+
     </div>
 
   );
---
+++ src/Header/header.js
@@ -0,0 +1,25 @@
+import logo from '../img/PICK.PNG'
+import './header.css';
+import menubar from '../img/Vector.png'
+import human from '../img/human.png'
+import alarm from '../img/Alarm.png'
+
+
+function header() {
+    return (
+        <div>
+            <div className='header'>
+                <div class="item"><img src={menubar} alt="menu" id="menubar"></img></div>
+                <div class="item"><img src={logo} alt="logo" id="logo"></img></div>
+
+                <div className="item">
+                    <img src={alarm} alt="logo" id="alarm"></img>
+                    <img src={human} alt="logo" id="human"></img>
+                </div>
+            </div>
+            <hr></hr>
+        </div>
+    );
+}
+
+export default header;
