1a935959a28bf55f13c39a0203e2f024575767ef
김지연 <kdelay20@gmail.com>
2022-09-23 16:05:13 +0900

    [게시판] 옷 추천 게시판
    db 연동해서 제목, 내용 표시 완료
    게시물이 2개씩 나오도록 한다.

--- src/Community/Pick/NoticeBoard/ClothRecommend.jsx
+++ src/Community/Pick/NoticeBoard/ClothRecommend.jsx
@@ -1,48 +1,45 @@
-// import React, { useState, useEffect } from 'react';
-// import Axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../Header/community_header'
 import './CSSClothRecommend.css'
 import { AiFillSkin, AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { BiMessage } from 'react-icons/bi';
+import TestMethod from "../../../Test/TestMethod";
 
 
 const Freedom = () => {
+    const [scrapChecked, setScrapChecked] = useState(true);
 
-    const [scrapChecked, setScrapChecked] = useState({
-        number1: false,
-        number2: false
-    });
+    const [community, setCommunity] = useState([]);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [click, setClick] = useState(true);
 
-    const { number1, number2 } = scrapChecked;
+    useEffect(() => {
+        const get = TestMethod.CommunityTestListGet()
+        const getData = () => {
+            get.then(data => {
+                setCommunity(data);
+                // setTitle(get.data[0].title)
+            });
+        };
+        getData();
+    }, [click]);
 
-    const handleScrapButton = (e) => {
-        setScrapChecked({
-            ...scrapChecked,
-            [e.target.name]: e.target.value
-        });
+    // const ontext = async () => {
+    //     await TestMethod.CommunityTestListPost(title, content)
+    //     if (click) {
+    //         setClick(false);
+    //     } else {
+    //         setClick(true);
+    //     }
+    //     setTitle("")
+    //     setContent("")
+    // }
 
-        console.log(e.target.name);
-
-        switch (e.target.name) {
-            case '1':
-                setScrapChecked({
-                    ...scrapChecked,
-                    [e.target.value]: true
-                });
-                console.log(number1);
-                break;
-
-            case '2':
-                setScrapChecked({
-                    ...scrapChecked,
-                    [e.target.value]: true
-                });
-                console.log(number2);
-                break;
-        }
-        // scrapChecked ? setScrapChecked(false) : setScrapChecked(true);
+    const handleScrapButton = () => {
+        scrapChecked ? setScrapChecked(false) : setScrapChecked(true);
     }
+
 
     return (
         <div>
@@ -51,66 +48,36 @@ const Freedom = () => {
                 <AiFillSkin size="80" />
                 <h1 className='text-margin-left-10'>옷 추천</h1>
             </div>
-            <div className='box'>
-                <div className='noticeBoard border-right'>
-                    <div className='text-margin-left-10'>
-                        <h2>제목</h2>
-                        <h4>내용 한 줄</h4>
+
+            <div className='box inline-block'>
+                {community.map((data) => (
+                    <div className='two-box'>
+                        <div className='noticeBoard'>
+                            <div className='text-margin-left-10'>
+                                <tr key={data.id}>
+                                    <tr><h2>{data.title}</h2></tr>
+
+                                    <tr><h3>{data.content}</h3></tr>
+                                </tr>
+                            </div>
+
+                            <h5 className='text-margin-left-10 text-margin-top-150'>작성자</h5>
+
+                            <div className='text-margin-left-10 flex'>
+                                <BiMessage size='20' className='text-top-2' />
+                                <h4 className='text-margin-left-10'>3</h4>
+                                <h4 className='text-margin-left-30'>3분 전</h4>
+
+                                {scrapChecked ?
+                                    <AiOutlineHeart size='35' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} /> :
+                                    <AiFillHeart size='35' color='red' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} />}
+                            </div>
+                            {/* <div className='border-right'></div> */}
+                        </div>
                     </div>
-                    <h5 className='text-margin-left-10 text-margin-top-150'>작성자</h5>
-                    <div className='text-margin-left-10 flex'>
-                        <BiMessage size='20' className='text-top-2' />
-                        <h4 className='text-margin-left-10'>3</h4>
-                        <h4 className='text-margin-left-30'>3분 전</h4>
-
-                        {scrapChecked ?
-                            <AiOutlineHeart number1='1' value={number1} size='35' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} /> :
-                            <AiFillHeart number1='1' value={number1} size='35' color='red' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} />}
-
-                    </div>
-
-                </div>
-
-                <div className='noticeBoard'>
-                    <div className='text-margin-left-10'>
-                        <h2>제목</h2>
-                        <h4>내용 한 줄</h4>
-                    </div>
-                    <h5 className='text-margin-left-10 text-margin-top-150'>작성자</h5>
-                    <div className='text-margin-left-10 flex'>
-                        <BiMessage size='20' className='text-top-2' />
-                        <h4 className='text-margin-left-10'>3</h4>
-                        <h4 className='text-margin-left-30'>3분 전</h4>
-
-                        {scrapChecked ?
-                            <AiOutlineHeart number2='2' value={number2} size='35' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} /> :
-                            <AiFillHeart number2='2' value={number2} size='35' color='red' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} />}
-                        {/* {scrapChecked ?
-                            <AiOutlineHeart size='35' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} /> :
-                            <AiFillHeart size='35' color='red' className='text-top-1 text-right text-margin-right-10 scrap' onClick={handleScrapButton} />} */}
-                    </div>
-
-                </div>
+                ))}
             </div>
-
-            <div className='box'>
-                <div className='noticeBoard border-right'>
-                    하이
-                </div>
-                <div className='noticeBoard'>
-                    하이
-                </div>
-            </div>
-
-            <div className='box'>
-                <div className='noticeBoard border-right'>
-                    하이
-                </div>
-                <div className='noticeBoard'>
-                    하이
-                </div>
-            </div>
-        </div>
+        </div >
     );
 };
 
