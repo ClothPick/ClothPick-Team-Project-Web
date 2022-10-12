04ec3918f13595c2f9c51a03b38e5ccd6bab29c1
김지연 <kdelay20@gmail.com>
2022-10-12 17:20:51 +0900

    [게시물] 댓글 component 병합

--- src/Community/Pick/Comment.jsx
+++ src/Community/Pick/Comment.jsx
@@ -4,22 +4,16 @@ import CommentForm from "./CommentForm";
 import CommentTableList from "./CommentTableList";
 
 function Comment() {
-    // const [id, setId] = useState(0);
-
     const [users, setUsers] = useState([])
 
     const handleCreate = (data) => {
-        // setId(id + 1);
-        // setUsers(users.concat(data, id));
         setUsers(users.concat(data));
-
         console.log(data);
-        // console.log(id);
     };
     return (
         <>
-            <CommentForm onCreate={handleCreate} />
-            <CommentTableList users={users} />
+            <CommentForm onCreate={handleCreate} users={users} />
+            {/* <CommentTableList users={users} /> */}
         </>
     );
 }
--- src/Community/Pick/CommentForm.jsx
+++ src/Community/Pick/CommentForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './NoticeBoard/CSSClothRecommend.css'
 import uuid from "react-uuid";
+import { FaTrashAlt } from 'react-icons/fa'
 
 class CommentForm extends Component {
 
@@ -8,15 +9,11 @@ class CommentForm extends Component {
         name: "",
         comment: "",
         id: uuid(),
-        // info: [],
+        showButton: false,
     };
 
+
     handleChange = (e) => {
-
-        // this.setState({
-        //     [e.target.name]: e.target.value,
-        // });
-
         // 입력한 value값을 각각 name과 comment에 저장한다.
         this.setState({
             [e.target.name]: e.target.value,
@@ -37,19 +34,6 @@ class CommentForm extends Component {
             }
         }
 
-        // newcomment에 comment와 고유 값 id를 생성한다.
-        let newcomment = {
-            name: this.state.name,
-            comment: this.state.comment,
-            id: this.state.id,
-            // info: this.state.info,
-        };
-
-        // addcomments에 [info, newcomment] 배열을 배열로 저장한다.
-        // let addcomments = [...this.state.info, newcomment];
-        // // info에 addcomments의 값을 저장하고
-        // this.setState({ info: addcomments });
-
         // 작성될 때마다 고유 값이 변경되도록 한다.
         this.setState({ id: uuid() });
 
@@ -61,28 +45,52 @@ class CommentForm extends Component {
             name: "",
             comment: "",
         });
+
+        console.log("값 : " + JSON.stringify(this.state));
+
     };
 
     render() {
+        const { users } = this.props;
+
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    name="name"
-                    onChange={this.handleChange}
-                    value={this.state.name}
-                    placeholder="작성자"
-                />
-                <div className="flex">
+            <>
+                <form onSubmit={this.handleSubmit}>
                     <input
-                        className="box-comment"
-                        name="comment"
+                        name="name"
                         onChange={this.handleChange}
-                        value={this.state.comment}
-                        placeholder="댓글을 입력하세요."
+                        value={this.state.name}
+                        placeholder="작성자"
                     />
-                    <button className='brown-round scrap' type="submit">작성</button>
+                    <div className="flex">
+                        <input
+                            className="box-comment"
+                            name="comment"
+                            onChange={this.handleChange}
+                            value={this.state.comment}
+                            placeholder="댓글을 입력하세요."
+                        />
+                        <button className='brown-round scrap' type="submit">작성</button>
+                    </div>
+                </form>
+
+                <div>
+                    {users && users.map((user =>
+                        <div key={user.id} >
+                            <div className="comment" onMouseOver={() => this.setState({ showButton: true })} onMouseOut={() => this.setState({ showButton: false })}>
+                                {
+                                    this.showButton ? <FaTrashAlt className="del-button" size='23' /> : <></>
+                                    // this.showButton ? <FaTrashAlt className="del-button" size='23' onClick={handleRemove(user.id)} /> : <></>
+                                }
+                                <div className="comment-text">{user.name}</div>
+                                <div className="comment-text2">{user.comment}</div>
+                                <div className="comment-time">2022-10-12 04:26:15</div>
+                                <button className="comment-btn">답글</button>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            </form>
+            </>
         );
     }
 }
