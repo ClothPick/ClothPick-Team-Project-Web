38d803015f22ea027bfe29d93185e33d501c9eb2
김지연 <kdelay20@gmail.com>
2022-10-21 11:42:24 +0900

    [게시판] 메인 페이지 db, css

--- src/Community/Main/Back.js
+++ src/Community/Main/Back.js
@@ -1,53 +1,64 @@
-import { AiOutlineAliwangwang } from 'react-icons/ai'
-import { AiFillLike } from 'react-icons/ai';
-
+import { AiOutlineAliwangwang, AiFillLike } from 'react-icons/ai'
+import { RiArrowRightSLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import TestMethod from '../../Test/TestMethod';
 
 import './Back.css'
 
 function Back() {
+    const [community, setCommunity] = useState([]);
+    const [click, setClick] = useState(true);
+
+    useEffect(() => {
+        const get = TestMethod.CommunityTestListGet();
+        const getData = () => {
+            get.then(data => {
+                setCommunity(data);
+                console.log(data)
+            });
+        };
+        getData();
+    }, [click]);
+
+    const showTitle = () => {
+        var arr = [];
+        var count = 1;
+        for (var i = 0; i < community.length; i++) {
+            if (count < 7) { // 게시물 개수가 1~6개일 때까지만 실행
+                if (community[i].boardType === '3') { // 게시판이 자유게시판일 경우
+                    arr.push(
+                        <div>
+                            <tr className='content2'>
+                                <div className='small-up'><td><AiFillLike /></td></div>
+                                {/* 추천 수 추후 수정 */}
+                                <td className='up-number'>0</td>
+                                {community[i].boardTitle}
+                            </tr>
+                        </div>
+                    )
+                    count++; // 게시물이 +1개 추가되었다.
+                }
+            }
+        }
+        return arr;
+    }
+
     return (
         <div className='free_total'>
             <div className='free'>
                 <div className='free_head'>
                     <AiOutlineAliwangwang size="70px"></AiOutlineAliwangwang>
                     <span>자유 게시판</span>
+                    <Link to="/freedom" className="etc">
+                        <p>더보기<RiArrowRightSLine /></p>
+                    </Link>
+
                 </div>
                 <hr id='hr2'></hr>
-
-                <div className='free_list'>
+                <div className='pick_list'>
                     <table id='freelist'>
-                        <tr>
-                            <td>이게 왜 이러지?</td>
-                        </tr>
-                        <tr>
-                            <td>이게 왜 이러지?</td>
-                        </tr>
-                        <tr>
-                            <td>이게 왜 이러지?</td>
-                        </tr>
-                        <tr>
-                            <td>이게 왜 이러지?</td>
-                        </tr>
-                        <tr>
-                            <td>이게 왜 이러지?</td>
-                        </tr>
-                    </table>
-                    <table id='good'>
-                        <tr>
-                            <td>.....</td>
-                        </tr>
-                        <tr>
-                            <td>.....</td>
-                        </tr>
-                        <tr>
-                            <td>.....</td>
-                        </tr>
-                        <tr>
-                            <td>.....</td>
-                        </tr>
-                        <tr>
-                            <td>.....</td>
-                        </tr>
+                        {showTitle()}
                     </table>
                 </div>
 
--- src/Community/Main/Pick.js
+++ src/Community/Main/Pick.js
@@ -1,23 +1,50 @@
 import './Pick.css'
 import { AiFillSkin, AiFillLike } from 'react-icons/ai';
 import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
+// import Axios from 'axios'
+import { RiArrowRightSLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+import TestMethod from '../../Test/TestMethod';
+
 
 
 const Pick = () => {
-    const [list, setList] = useState([]);
-    useEffect(() => {
-        Axios.get("/api/list3").then((response) => {
-            setList(response.data)
-        })
-    }, []);
+    const [community, setCommunity] = useState([]);
+    const [click, setClick] = useState(true);
 
-    const [rank, setrank] = useState([]);
     useEffect(() => {
-        Axios.get("/api/clothesRank").then((response) => {
-            setrank(response.data)
-        })
-    }, [])
+        const get = TestMethod.CommunityTestListGet();
+        const getData = () => {
+            get.then(data => {
+                setCommunity(data);
+                console.log(data)
+            });
+        };
+        getData();
+    }, [click]);
+
+    const showTitle = () => {
+        var arr = [];
+        var count = 1;
+        for (var i = 0; i < community.length; i++) {
+            if (count < 7) { // 게시물 개수가 1~6개일 때까지만 실행
+                if (community[i].boardType === '1') { // 게시판이 옷 추천 게시판일 경우
+                    arr.push(
+                        <div>
+                            <tr className='content2'>
+                                <div className='small-up'><td><AiFillLike /></td></div>
+                                {/* 추천 수 추후 수정 */}
+                                <td className='up-number'>0</td>
+                                {community[i].boardTitle}
+                            </tr>
+                        </div>
+                    )
+                    count++; // 게시물이 +1개 추가되었다.
+                }
+            }
+        }
+        return arr;
+    }
 
     return (
         <div className='pick_total'>
@@ -25,34 +52,15 @@ const Pick = () => {
                 <div className='pick_head2'>
                     <AiFillSkin size="70"></AiFillSkin>
                     <p>옷 추천</p>
+                    <Link to="/recommend" className='etc'>
+                        <span>더보기<RiArrowRightSLine /></span>
+                    </Link>
                 </div>
                 <hr />
                 <div className='pick_list'>
                     <table id='pick_list'>
-                            {
-                                list.map((data) => (
-                                        <tr className='content1' key={data.id}>
-                                            <td>{data.title}</td>
-                                        </tr>
-                                ))
-                            }
+                        {showTitle()}
                     </table>
-
-                    <div className='choice'>
-                        <table id='choice'>
-                            <tbody>
-                                {
-                                    list.map((data) => (
-                                        <tr className='content2' key={data.id}>
-                                            <AiFillLike id='small_up'></AiFillLike>
-                                            <td>{data.countLike}</td>
-                                        </tr>
-
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
             </div>
 
@@ -83,11 +91,11 @@ const Pick = () => {
                         <table id='pick_rank_content'>
                             <tbody>
                                 {
-                                    rank.map((data) => (
-                                        <tr id='rank1' key={data.id}>
-                                            <td>{data.content}</td>
-                                        </tr>
-                                    ))
+                                    // rank.map((data) => (
+                                    //     <tr id='rank1' key={data.id}>
+                                    //         <td>{data.content}</td>
+                                    //     </tr>
+                                    // ))
                                 }
                             </tbody>
                         </table>
