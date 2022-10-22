442afbaca89b538fb50c804fe6f67bcdb60f50e1
김지연 <kdelay20@gmail.com>
2022-10-22 20:42:59 +0900

    [내 옷장] 이미지 출력

--- src/Closet/ClosetList/closetList.js
+++ src/Closet/ClosetList/closetList.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './closetList.css'
-import cloth1 from '../../img/cloth1.png'
 import Accordion from '../Accordion/Accordion';
 import "../Accordion/Accordion.css";
 import { IoMdArrowDropleftCircle } from "react-icons/io"
 import { IoMdArrowDroprightCircle } from "react-icons/io"
 import ClosetMethod from '../../Test/ClosetMethod';
+import data from '../Accordion/data';
 
 
 function ClosetList() {
@@ -18,9 +18,11 @@ function ClosetList() {
     const [pageNumbers, setPageNumbers] = useState([]);
 
     const [clothImg, setClothImg] = useState([]);
+    const [clothInfo, setClothInfo] = useState([]);
 
     useEffect(() => {
         const get = ClosetMethod.ConnectClosetImgGet();
+        const closetInfo = ClosetMethod.ClosetInfoGet();
         const abc = [];
 
         const getData = () => {
@@ -34,21 +36,14 @@ function ClosetList() {
                 }
                 setPageNumbers(abc);
             });
+
+            closetInfo.then(data => {
+                setClothInfo(data);
+                console.log(data);
+            });
         };
         getData();
     }, [currentPage]);
-
-    // useEffect(() => {
-
-    //     const abc = [];
-    //     setOnepae(list.slice((currentPage - 1) * 6, (currentPage - 1) * 6 + 6))
-    //     for (var i = 1; i <= Math.ceil(list.length / postsPerPage); i++) {
-    //         abc.push(i);
-    //     }
-    //     setPageNumbers(abc)
-    // }, [currentPage]);
-
-
 
     const oncheck = (data) => {
         console.log('확인' + data);
@@ -70,154 +65,6 @@ function ClosetList() {
         }
     }
 
-
-
-    const list = [
-        {
-            id: 1,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-
-        },
-        {
-            id: 2,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '체크 난방'
-        },
-        {
-            id: 3,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '내가 좋아하는 니트'
-        },
-        {
-            id: 4,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '디스 이즈'
-        },
-        {
-            id: 5,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '베이지 반팔티'
-        },
-        {
-            id: 6,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '푸마 반팔'
-        },
-        {
-            id: 7,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '반팔인데 로고 민트 7'
-        },
-        {
-            id: 8,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '언제 샀는지 기억 안나  8'
-        },
-        {
-            id: 9,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '커버낫123 9'
-        },
-        {
-            id: 10,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '커버낫'
-        },
-        {
-            id: 11,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 12,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 13,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 14,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 15,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 16,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 17,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 18,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 19,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 20,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 21,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 22,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 23,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 24,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 25,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 26,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 27,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        },
-        {
-            id: 28,
-            img: <img src={cloth1} alt=""></img>,
-            explain: '기모 긴팔 티'
-        }
-
-
-    ];
-
     return (
         <div className='root'>
             <Accordion />
@@ -225,16 +72,18 @@ function ClosetList() {
                 <div className='itemlist'>
                     {
                         onepage.map(data => (
-                            // <div key={data.clothImgName}>
-                            // </div>
-                            <img alt='' src={`http://192.168.0.109:80/api/displayimg/${data.clothImgName}.jpg`} />
+                            <span key={data.clothImgName}>
+                                <img alt="" className="" src={`http://192.168.0.101:8080/api/v1/displayimg/closet/${data.clothImgName}`} />
+                            </span>
                         ))
-                        // onepage.map(data => (
-                        //     <div key={data.id}>
-                        //         <div className='itemimg'>{data.img}</div>
-                        //         <div className='itemname'>{data.explain}</div>
-                        //     </div>
-                        // ))
+                    }
+
+                    {
+                        clothInfo.map(data => (
+                            <span key={data.clothId}>
+                                <h3>{data.clothKeyword}</h3>
+                            </span>
+                        ))
                     }
                 </div>
                 <div className='num'>
